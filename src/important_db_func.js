a54c120520007f86202f6a3041c021f4d0e416a1
Chidozie <codingwithcn@gmail.com>
2021-09-22 16:29:03 -0400

    Fixing database error from prelease 1.7.0

--- src/important_db_func.js
+++ src/important_db_func.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 var fs = require('fs');
 const path = require('path');
-const db_file_loc = path.join(__dirname, "client.db")
+const db_file_loc = path.resolve(process.resourcesPath, "databaseFolder/client.db")
 
 /**
  * Checks if table exist in database, if it does not, 
